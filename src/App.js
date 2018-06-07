22642309db2e364f4c1fd0161946bfa677519fcf
Richard Temple <richardjamestemple@gmail.com>
2018-06-06 19:37:07 -0700

    get to funtional components

--- src/App.js
+++ src/App.js
@@ -68,19 +68,16 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={this.onSearchChange}
-        />
-      </form>
-    );
-  }
+function Search({value, onChange, children}) {
+  return (
+    <form>
+      {children} <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  );
 }
 
 class Table extends Component {
@@ -97,15 +94,33 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={() => onDismiss(item.objectID)}
-                  type="button">
-                  Dismiss
-              </button>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
             </span>
           </div>
         )}
       </div>
+    );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
     );
   }
 }
